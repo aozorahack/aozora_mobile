e78fac85112fceda86bb26d7ec46242130e5814f
takuma aota <aoitaku@gmail.com>
2016-07-30 18:24:08 +0900

    refactoring

--- app/book.rb
+++ app/book.rb
@@ -5,13 +5,15 @@ module Aozora
     attr_reader :body
 
     def initialize(person_id, book_id, book_format_id)
-      path = %{cards/#{person_id}/files/#{book_id}_#{book_format_id}}
+      dir = %{cards/#{person_id}/files}
+      path = %{#{dir}/#{book_id}_#{book_format_id}}
 
       fetcher = Fetcher.new(path)
       response = fetcher.fetch
       @body = response.css('body')
-      imgs = @body.css('.main_text img')
-      imgs.map {|img| img['src'] = "http://www.aozora.gr.jp/cards/#{person_id}/files/#{img['src']}" }
+      @body.css('.main_text img').map do |img|
+        img['src'] = %{#{Fetcher::BASE_URI}/#{dir}/#{img['src']}}
+      end
     end
 
   end
