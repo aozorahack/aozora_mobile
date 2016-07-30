25dee2280a66d7c11313e11d0e6a9179fa9574a1
takuma aota <aoitaku@gmail.com>
2016-07-30 16:04:53 +0900

    add book view

---
+++ app/book.rb
@@ -0,0 +1,10 @@
+module Aozora
+
+  class Book
+
+    def initialize(person_id, book_id, book_format_id)
+    end
+
+  end
+
+end
--- app/main.rb
+++ app/main.rb
@@ -1,3 +1,10 @@
 get '/' do
   haml :index
 end
+
+get %r{cards/(\d{6})/files/(\d+)_(\d+)} do |person_id, book_id, book_format_id|
+  book = Book.new(person_id, book_id, book_format_id)
+  haml :book, locals: {
+    book: book
+  }
+end
