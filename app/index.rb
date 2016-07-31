31475ed511415011ec9690da31e1ebfc5c28cd1c
Koki Takahashi <hakatasiloving@gmail.com>
2016-07-31 12:19:29 +0900

    Get latest work information from aozora official page

--- app/index.rb
+++ app/index.rb
@@ -2,11 +2,25 @@ module Aozora
 
   class Index
 
-    attr_reader :dom
+    attr_reader :dom, :info
 
     def initialize
       fetcher = Aozora::Fetcher.new('')
-      @dom = fetcher.fetch
+      @dom = fetcher.fetch('utf-8')
+      get_info
+    end
+
+    def get_info
+      @info = {}
+
+      information_el = @dom.at_css('[summary=information]')
+
+      latest_work_el = information_el.css('tr').find do |tr|
+        tr.children.at('td.subheader')&.text&.include?('最新公開作品')
+      end
+
+      @info[:latest_work_name] = latest_work_el.at_css('td.summary').text
+      @info[:latest_work_url] = latest_work_el.at_css('td.summary > a').attribute('href')
     end
 
   end
